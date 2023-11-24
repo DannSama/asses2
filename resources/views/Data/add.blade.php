33d62033fa70ec38fcbb7076a259498530acadf3
Athaya Hanif <96409853+DannSama@users.noreply.github.com>
2023-11-24 15:50:24 +0700

    first commit

---
+++ resources/views/Data/add.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Add New Data</h1>
+
+        <div class="container mt-5">
+  <form>
+    <div class="mb-3">
+      <label for="exampleInput" class="form-label text-primary">Name</label>
+      <input type="text" class="form-control" id="exampleInput" placeholder="Enter your text">
+    </div>
+  </form>
+</div>    
+
+    <div class="container mt-5">
+  <form>
+    <div class="mb-3">
+      <label for="exampleTextarea" class="form-label">Details</label>
+      <textarea class="form-control" id="exampleTextarea" rows="5"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</div>
+
+    </div>
+    
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> 
+</body>
+</html>
